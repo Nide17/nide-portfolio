aa9fa759e4d9cdff458144fadd3ae45fb6560610
Nide17 <nidedrogba@gmail.com>
2021-12-20 21:29:29 +0200

    home started

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,12 +1,35 @@
 import Image from 'next/image'
+import welcomeStyles from '../styles/Welcome.module.css'
 
 export default function Welcome() {
     return (
-        <div className="welcome" id="home">
+        <div className={welcomeStyles.welcome} id="home">
 
-            <Image src="/images/NIDEiMAGE.JPG" alt="nideimage" width={200} height={200} />
-            <p>Hi, I am Niyomwungeri Parmenide Ishimwe
-                <br />
-                I am a Software Developer, Software Designer, IT Support Engineer from Kigali, Rwanda.</p>
+            <div className="myimage">
+                <Image src="/images/NIDEiMAGE.JPG" alt="nideimage" width={280} height={280} />
+            </div>
+
+            <div className="my-text">
+                <p>Hi, I am Niyomwungeri Parmenide Ishimwe</p>
+
+                <p className={welcomeStyles.typewriter}>
+                    I am a
+
+                    <br />
+                    <div className={welcomeStyles.dev}>
+                        <div className="developer">Web Developer</div></div>
+
+                    <br />
+                    <div className={welcomeStyles.des}>
+                        <div className="designer">Software Designer</div></div>
+
+                    <br />
+                    <div className={welcomeStyles.eng}>
+                        <div className="engineer">IT Support Engineer</div></div>
+
+                    <br />
+                </p>
+
+            </div>
         </div>)
 };
