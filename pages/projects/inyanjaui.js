939bbbc2f0747e9db1a9bafdaed036c806ac2594
Nide17 <nidedrogba@gmail.com>
2021-12-25 01:58:07 +0200

    all projects

--- pages/projects/inyanjaui.js
+++ pages/projects/inyanjaui.js
@@ -22,9 +22,7 @@ export default function inyanjaui() {
 
                     <h2>D<span>escription</span></h2>
 
-                    <p>This web application was made Elysium Group, a company that has been established in November 2019 to provide Consultancy services in Architectural and Engineering activities and related technical consultancy, Consultancy in Project Management and Estate Construction, Consultancy in the field of construction, Control and supervision of works and real estate appraisals Research and experimental development on natural sciences and engineering, Technical testing and analysis, Specialized design activities, ICT Consultancy, Business development and management consultancy, accounting, Auditing, book-keeping Consultancy and other scientific research.</p>
-
-                    <p>This web application helps this company for online presense and displays their services, projects, team and various contact ways.</p>
+                    <p>This front-end web application was built to manage Inyanja Space Learning UI Project that was being developed but it has now stopped.</p>
                 </div>
 
                 <div className={projectStyles.pImages}>
@@ -32,7 +30,7 @@ export default function inyanjaui() {
 
                     <div className={projectStyles.techNvisit}>
 
-                        <p>Notable Technologies: <span>React.js, Redux, Node.js, Express.js, MongoDB, Reactstrap, Bootstrap, More ...</span></p>
+                        <p>Notable Technologies: <span>React.js, Redux, Bootstrap, More ...</span></p>
                         <Link href="http://inyanjaui.herokuapp.com/">
                             <a>Visit Inyanja Web Space UI</a>
                         </Link>
--- pages/projects/paypal2.js
+++ pages/projects/paypal2.js
@@ -22,9 +22,9 @@ export default function paypal2() {
 
                     <h2>D<span>escription</span></h2>
 
-                    <p>This web application was made Elysium Group, a company that has been established in November 2019 to provide Consultancy services in Architectural and Engineering activities and related technical consultancy, Consultancy in Project Management and Estate Construction, Consultancy in the field of construction, Control and supervision of works and real estate appraisals Research and experimental development on natural sciences and engineering, Technical testing and analysis, Specialized design activities, ICT Consultancy, Business development and management consultancy, accounting, Auditing, book-keeping Consultancy and other scientific research.</p>
+                    <p>This front-end app was built with React.js to mimic PayPal&apos;s website</p>
 
-                    <p>This web application helps this company for online presense and displays their services, projects, team and various contact ways.</p>
+                    <p>This web application was done to mimic the real paypal website with react.js.</p>
                 </div>
 
                 <div className={projectStyles.pImages}>
@@ -32,7 +32,7 @@ export default function paypal2() {
 
                     <div className={projectStyles.techNvisit}>
 
-                        <p>Notable Technologies: <span>React.js, Redux, Node.js, Express.js, MongoDB, Reactstrap, Bootstrap, More ...</span></p>
+                        <p>Notable Technologies: <span>React.js, HTML, CSS, Bootstrap, More ...</span></p>
                         <Link href="http://paypal2.herokuapp.com/">
                             <a>Visit Paypal2</a>
                         </Link>
