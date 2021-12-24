004b6adb9ae1485308819b7558c2e4a356e7c941
Nide17 <nidedrogba@gmail.com>
2021-12-24 17:20:39 +0200

    contact responsive

--- components/Contact.js
+++ components/Contact.js
@@ -1,19 +1,46 @@
+import Image from 'next/image'
 import contactStyles from '../styles/Contact.module.css'
 
 export default function Contact() {
     return (
-        <div className="contact" id="contact">
-            <div className="contact-text">
-                Want to discuss about a project or something else? Do not hesitate to contact me, I am always open to hear from you!
+        <div className={contactStyles.contact} id="contact">
+
+            <div className={contactStyles.contactDetails}>
+
+                <h2>C<span>ontact Me</span></h2>
+
+                <div className="contactText">
+
+                    <p>
+                        &quot;Want to discuss about a project, collaborate or something else? Do not hesitate to contact me, I am always open to hear from you!&quot;</p>
+                </div>
+
+                <div className={contactStyles.reachMe}>
+
+                    <div className={contactStyles.reachMeImg}>
+                        <Image src="/images/placeholder.png" alt='placeholder' width={30} height={30} />
+                        <span className={contactStyles.imgSpan}>Kigali, Rwanda</span>
+                    </div>
+
+                    <div className={contactStyles.reachMeImg}>
+                        <Image src="/images/gmail.png" alt='gmail' width={30} height={30} />
+                        <span className={contactStyles.imgSpan}>nidedrogba@gmail.com</span>
+                    </div>
+                    <div className={contactStyles.reachMeImg}>
+                        <Image src="/images/whatsapp.png" alt='whatsapp' width={30} height={30} />
+                        <span className={contactStyles.imgSpan}>+250788551997</span>
+                    </div>
+                </div>
+
             </div>
 
-            <div className="contact-form">
-                <form action="">
-                    <div className="input-row">
-                        <input type="text" className="name" />
-                        <input type="email" className="email" />
-                        <textarea name="message" id="" cols="30" rows="10" className="message"></textarea>
-                        <button className="send-message">Send</button>
+            <div className={contactStyles.contactForm}>
+                <form>
+                    <div className="inputRow">
+                        <input type="text" className="name" placeholder='Your Name ...'/>
+                        <input type="email" className="email" placeholder='Your Email ...'/>
+                        <textarea name="message" id="" cols="30" rows="6" className="message" placeholder='Your Message ...'></textarea>
+                        <button className="send-message">Send Message</button>
                     </div>
                 </form>
             </div>
