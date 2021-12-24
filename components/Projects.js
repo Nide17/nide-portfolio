333fd93c25c5097f860d7a50e2179fd3a88e1f29
Nide17 <nidedrogba@gmail.com>
2021-12-24 13:58:23 +0200

    all projects w/out resp

--- components/Projects.js
+++ components/Projects.js
@@ -7,21 +7,24 @@ export default function Projects() {
     return (
         <div className={projectsStyles.projects} id="projects">
 
+            <h2>P<span>rojects</span></h2>
+
             <div className={projectsStyles.projectsList}>
 
                 <div className={projectsStyles.listItem}>
                     <Link href="/projects/elysiumgroupltd">
                         <a>
-                            <Image src='/images/elysiumgroupltd.JPG' alt='Elysium Group Ltd' width={200} height={200}></Image>
+                            <Image src='/images/elysiumgroupltd.JPG' alt='Elysium Group Ltd' width={350} height={170}></Image>
+
+                            <h5>Elysium Group Ltd</h5>
 
                             <div className={projectsStyles.toolsBtn}>
                                 <button className="tools-round">React.js</button>
                                 <button className="tools-round">Node.js</button>
                                 <button className="tools-round">Express.js</button>
                                 <button className="tools-round">MongoDB</button>
+                                <button className="tools-round">Bootstrap</button>
                             </div>
-
-                            <h3>Elysium Group Ltd</h3>
                         </a>
                     </Link>
                 </div>
@@ -29,16 +32,17 @@ export default function Projects() {
                 <div className={projectsStyles.listItem}>
                     <Link href="/projects/quizblog">
                         <a>
-                            <Image src='/images/quizblog.JPG' alt='Quiz Blog' width={200} height={200}></Image>
+                            <Image src='/images/quizblog.JPG' alt='Quiz Blog' width={350} height={170}></Image>
+
+                            <h5>Quiz Blog</h5>
 
                             <div className={projectsStyles.toolsBtn}>
                                 <button className="tools-round">React.js</button>
                                 <button className="tools-round">Node.js</button>
                                 <button className="tools-round">Express.js</button>
                                 <button className="tools-round">MongoDB</button>
+                                <button className="tools-round">Bootstrap</button>
                             </div>
-
-                            <h3>Quiz Blog</h3>
                         </a>
                     </Link>
                 </div>
@@ -46,16 +50,70 @@ export default function Projects() {
                 <div className={projectsStyles.listItem}>
                     <Link href="/projects/okaziyo">
                         <a>
-                            <Image src='/images/okaziyo.JPG' alt='Okaziyo' width={200} height={200}></Image>
+                            <Image src='/images/okaziyo.JPG' alt='Okaziyo' width={350} height={170}></Image>
+
+                            <h5>Okaziyo</h5>
 
                             <div className={projectsStyles.toolsBtn}>
                                 <button className="tools-round">React.js</button>
                                 <button className="tools-round">Node.js</button>
                                 <button className="tools-round">Express.js</button>
                                 <button className="tools-round">MongoDB</button>
+                                <button className="tools-round">Bootstrap</button>
                             </div>
+                        </a>
+                    </Link>
+                </div>
 
-                            <h3>Okaziyo</h3>
+                <div className={projectsStyles.listItem}>
+                    <Link href="/projects/hortiprice">
+                        <a>
+                            <Image src='/images/hortiprice.JPG' alt='hortiprice' width={350} height={170}></Image>
+
+                            <h5>Hortiprice</h5>
+
+                            <div className={projectsStyles.toolsBtn}>
+                                <button className="tools-round">PHP</button>
+                                <button className="tools-round">MySQL</button>
+                                <button className="tools-round">Bootstrap</button>
+                                <button className="tools-round">HTML</button>
+                                <button className="tools-round">CSS</button>
+                            </div>
+                        </a>
+                    </Link>
+                </div>
+
+                <div className={projectsStyles.listItem}>
+                    <Link href="/projects/paypal2">
+                        <a>
+                            <Image src='/images/paypal2.JPG' alt='paypal2' width={350} height={170}></Image>
+
+                            <h5>Paypal2</h5>
+
+                            <div className={projectsStyles.toolsBtn}>
+                                <button className="tools-round">React.js</button>
+                                <button className="tools-round">HTML</button>
+                                <button className="tools-round">CSS</button>
+                                <button className="tools-round">Bootstrap</button>
+                            </div>
+                        </a>
+                    </Link>
+                </div>
+
+                <div className={projectsStyles.listItem}>
+                    <Link href="/projects/inyanjaui">
+                        <a>
+                            <Image src='/images/inyanjaui.JPG' alt='inyanjaui' width={350} height={170}></Image>
+
+                            <h5>Inyanjaui</h5>
+
+                            <div className={projectsStyles.toolsBtn}>
+                                <button className="tools-round">React.js</button>
+                                <button className="tools-round">Redux</button>
+                                <button className="tools-round">Reactstrap</button>
+                                <button className="tools-round">Bootstrap</button>
+                                <button className="tools-round">HTML/CSS</button>
+                            </div>
                         </a>
                     </Link>
                 </div>
