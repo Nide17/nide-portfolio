c4f971241857f7fbadb0f30b1bc9fe402002a270
Nide17 <nidedrogba@gmail.com>
2021-12-25 01:42:03 +0200

    hortiprice

--- pages/projects/hortiprice.js
+++ pages/projects/hortiprice.js
@@ -22,9 +22,9 @@ export default function hortiprice() {
 
                     <h2>D<span>escription</span></h2>
 
-                    <p>Quiz Blog is a web application that provides a multi-category space for people to quiz from. It gives people good time to fix what they studied and even prepare for exams.</p>
+                    <p>Hortiprice is a web applicaion was built with Python/Flask to help NAEB/PRICE to store, retrieve info about horticulture farmers and their business plans</p>
 
-                    <p>This web application helps learners to view, attempt quizzes and review their answers. It contains also notes and past papers for various exams. On this app the user can attempt any quiz but answers and reviews are saved once he or she is logged in. He can also download notes and past papers when he is logged too.</p>
+                    <p>This web application helps the PRICE manager and the horticulture value chain specialist to view, add, update and delete farmers as well as their data. On this app the user can view only these details of farmers.</p>
                 </div>
 
                 <div className={projectStyles.pImages}>
@@ -32,7 +32,7 @@ export default function hortiprice() {
 
                     <div className={projectStyles.techNvisit}>
 
-                        <p>Notable Technologies: <span>React.js, Redux, Node.js, Express.js, MongoDB, Reactstrap, Bootstrap, More ...</span></p>
+                        <p>Notable Technologies: <span>Python, Flask, PostgreSQL, Jinja2, HTML, CSS, Bootstrap, More ...</span></p>
                         <Link href="http://hortiprice.herokuapp.com/">
                             <a>Visit Hortiprice</a>
                         </Link>
