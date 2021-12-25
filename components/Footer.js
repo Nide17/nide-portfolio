3b673d969d729a7077648b65ef757bfc5fa04391
Nide17 <nidedrogba@gmail.com>
2021-12-25 02:06:13 +0200

    social links

--- components/Footer.js
+++ components/Footer.js
@@ -23,19 +23,38 @@ export default function Footer() {
             </div>
 
             <hr />
-            
+
             <div className={footerStyles.rightPart}>
                 <small>
                     <blockquote>~Hard Work pays off~</blockquote>
                 </small>
 
                 <div className={footerStyles.socialIcons}>
-                    <Image src="/images/github.png" alt='github' width={18} height={18} />
-                    <Image src="/images/upwork.png" alt='upwork' width={18} height={18} />
-                    <Image src="/images/linkedin.png" alt='linkedin' width={18} height={18} />
-                    <Image src="/images/instagram.png" alt='instagram' width={18} height={18} />
-                    <Image src="/images/twitter.png" alt='twitter' width={18} height={18} />
-                    <Image src="/images/facebook.png" alt='facebook' width={18} height={18} />
+
+                    <Link href="https://github.com/Nide17"><a>
+                        <Image src="/images/github.png" alt='github' width={18} height={18} />
+                    </a></Link>
+
+                    <Link href="https://www.upwork.com/freelancers/~013f5da808a8367c20/"><a>
+                        <Image src="/images/upwork.png" alt='upwork' width={18} height={18} />
+                    </a></Link>
+
+                    <Link href="https://www.linkedin.com/in/niyomwungeri-parmenide-ishimwe-1a5394123/"><a>
+                        <Image src="/images/linkedin.png" alt='linkedin' width={18} height={18} />
+                    </a></Link>
+
+                    <Link href="https://www.instagram.com/ishimwe_parmenide"><a>
+                        <Image src="/images/instagram.png" alt='instagram' width={18} height={18} />
+                    </a></Link>
+
+                    <Link href="https://www.twitter.com/@ishimweparmenid"><a>
+                        <Image src="/images/twitter.png" alt='twitter' width={18} height={18} />
+                    </a></Link>
+
+                    <Link href="https://web.facebook.com/nide.drogba.7"><a>
+                        <Image src="/images/facebook.png" alt='facebook' width={18} height={18} />
+                    </a></Link>
+
                 </div>
 
                 <div className={footerStyles.copyright}>
