eae93fc149c9b15180ed943e4d1518eb619a1e6f
Nide17 <nidedrogba@gmail.com>
2022-01-20 19:08:50 +0200

    implemented the contact form

--- components/Contact.js
+++ components/Contact.js
@@ -1,11 +1,48 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import contactStyles from '../styles/Contact.module.css'
 
 export default function Contact() {
 
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    const onChangeHandler = e => {
+        const { name, value } = e.target
+        setState(state => ({ ...state, [name]: value }))
+    };
+
     const onSubmitHandler = (e) => {
         e.preventDefault();
-        console.log('Message sent!');
+
+        const { name, email, message } = state;
+
+        // Create user object
+        const contactMsg = {
+            name,
+            email,
+            message
+        };
+
+        fetch('https://nide-portfolio-server.herokuapp.com/contacts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(contactMsg),
+        })
+
+        console.log(contactMsg);
+
+        // Reset fields
+        setState({
+            name: '',
+            email: '',
+            message: ''
+        })
     }
 
     return (
@@ -41,13 +78,18 @@ export default function Contact() {
             </div>
 
             <div className={contactStyles.contactForm}>
-                <form>
-                    <div className="inputRow">
-                        <input type="text" className="name" placeholder='Your Name ...' required />
-                        <input type="email" className="email" placeholder='Your Email ...' required />
-                        <textarea name="message" id="" cols="30" rows="6" className="message" placeholder='Your Message ...' required></textarea>
 
-                        <button className="send-message" onClick={onSubmitHandler}>Send Message</button>
+                <form onSubmit={onSubmitHandler}>
+
+                    <div className="inputRow">
+                        <input name="name" type="text" className="name" placeholder='Your Name ...' onChange={onChangeHandler} value={state.name || ""} required />
+
+                        <input name="email" type="email" className="email" placeholder='Your Email ...' onChange={onChangeHandler} value={state.email || ""} required />
+
+                        <textarea name="message" id="" cols="30" rows="6" className="message" placeholder='Your Message ...' onChange={onChangeHandler} value={state.message || ""} required></textarea>
+
+                        <button className="send-message">Send Message</button>
+                        {/* <button className="send-message" onClick={onSubmitHandler}>Send Message</button> */}
                     </div>
                 </form>
             </div>
